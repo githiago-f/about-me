3889806b7525aa563e19698d49d6d4cbef7f40a4
githiago-f <tfarias@protonmail.com>
2021-04-25 18:46:25 -0300

    add: social media preview tags

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -6,7 +6,7 @@ export const NavBar = () => {
       <div className="flex justify-between">
         <a href="/about-me">
           <h3 className="font-bold p-1">
-            Portifólio <span className="bg-blue-600 px-2 text-white rounded-md">dev</span>
+            Portfolio <span className="bg-blue-600 px-2 text-white rounded-md">dev</span>
           </h3>
         </a>
       </div>
